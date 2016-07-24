b790ec13c59307aeb8c0de26d6789fac2371cc08
Christian Blanquera <cblanquera@openovate.com>
2016-07-24 09:00:10 +0800

    Backend gut out
    - Removed Nested Object Concept because it was too heavy when dealing
    with a lot of data
    - Favoured References and pointers over recursive calls
    - Added `getDot`, `setDot` and `isDot`
    - Updated test suites
    - still backwards compatible

--- test/Index.php
+++ test/Index.php
@@ -12,17 +12,31 @@ class EdenRegistryIndexTest extends PHPUnit_Framework_TestCase
     public function testSet() 
     {
 		$registry = eden('registry') //instantiate
-		->set('path', 'to', 'value1', 123)   //set path 'path','to', 'value' to 123
-		->set('path', 'to', 'value2', 456);  //set path 'path','to', 'value' to 456
+			->set('path', 'to', 'value1', 123)   //set path 'path','to', 'value' to 123
+			->set('path', 'to', 'value2', 456);  //set path 'path','to', 'value' to 456
 		
 		$this->assertInstanceOf('Eden\\Registry\\Base', $registry);
+		
+		//overriding
+		$results = $registry
+			->set('path', 'to', 'value1', 'value2', 123)
+			->getArray();
+		
+		$this->assertEquals(123, $results['path']['to']['value1']['value2']);
+		
+		//overriding
+		$results = $registry
+			->set('path', 123)
+			->getArray();
+		
+		$this->assertEquals(123, $results['path']);
 	}
 	
 	public function testGet() 
     {
 		$registry = eden('registry') //instantiate
-		->set('path', 'to', 'value1', 123)   //set path 'path','to', 'value' to 123
-		->set('path', 'to', 'value2', 456);  //set path 'path','to', 'value' to 456
+			->set('path', 'to', 'value1', 123)   //set path 'path','to', 'value' to 123
+			->set('path', 'to', 'value2', 456);  //set path 'path','to', 'value' to 456
 		
 		$value1 = $registry->get('path', 'to', 'value1'); //--> 123
 		$value2 = $registry->get('path', 'to', 'value2'); //--> 456
@@ -37,5 +51,63 @@ class EdenRegistryIndexTest extends PHPUnit_Framework_TestCase
 		$registry['name'] = 'value';
 		
 		$this->assertEquals('value', $registry['name']);
+		
+		$this->assertEquals(123, $registry['path']['to']['value1']);
+		$this->assertTrue(!isset($registry['path']['to']['value3']));
+		$this->assertNull($registry->get('path', 'to', 'value3', 'and', 'beyond'));
+		$this->assertEquals(123, $registry->path['to']['value1']);
+	}
+	
+	public function testRemove()
+	{
+		$registry = eden('registry') //instantiate
+			->set('path', 'to', 'value1', 123)   //set path 'path','to', 'value' to 123
+			->set('path', 'to', 'value2', 456);  //set path 'path','to', 'value' to 456
+		
+		$registry->remove('path', 'to', 'value1');
+		
+		$this->assertTrue(!isset($registry['path']['to']['value1']));
+		$this->assertNull($registry->get('path', 'to', 'value1'));
+		
+		$this->assertEquals('{"path":{"to":{"value2":456}}}', (string) $registry);
+	}
+	
+	public function testIsKey()
+	{
+		$registry = eden('registry') //instantiate
+			->set('path', 'to', 'value1', 123)   //set path 'path','to', 'value' to 123
+			->set('path', 'to', 'value2', 456);  //set path 'path','to', 'value' to 456
+		
+		$this->assertTrue($registry->isKey('path', 'to', 'value1'));
+		$this->assertTrue($registry->isKey('path', 'to', 'value2'));
+		$this->assertTrue($registry->isKey('path', 'to'));
+		$this->assertFalse($registry->isKey('path', 'to', 'value3'));
+	}
+	
+	public function testIsDot()
+	{
+		$registry = eden('registry') //instantiate
+			->set('path', 'to', 'value1', 123)   //set path 'path','to', 'value' to 123
+			->set('path', 'to', 'value2', 456);  //set path 'path','to', 'value' to 456
+		
+		$this->assertTrue($registry->isDot('path.to.value1'));
+		$this->assertTrue($registry->isDot('path-to-value2', '-'));
+		$this->assertTrue($registry->isDot('path.to'));
+		$this->assertFalse($registry->isDot('path.to.value3'));
+		$this->assertFalse($registry->isDot('path-to-value3'));
+	}
+	
+	public function testDot()
+	{
+		$registry = eden('registry')
+			->setDot('path.to.value1', 123)
+			->setDot('path-to-value2', 456, '-')
+			->setDot('path-to-value3', 789);
+			
+		$results = $registry->getDot('path.to');
+		
+		$this->assertEquals(123, $results['value1']);
+		$this->assertEquals(456, $results['value2']);
+		$this->assertTrue(!isset($results['value3']));
 	}
 }
